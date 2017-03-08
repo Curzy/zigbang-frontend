b5426781a954448ac672917ed9b4e519d98ad83f
Curzy <cms9597@gmail.com>
2017-03-09 05:06:54 +0900

    First Commit

---
+++ src/components/TopBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class TopBar extends Component {
+  render() {
+    return (
+      <div className="TopBar">
+        <div className="button back">
+          <button></button>
+        </div>
+        <div className="button share">
+          <button></button>
+        </div>
+        <div className="button like">
+          <button></button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TopBar;
